063e359a8c770bac13efa98a2f8dde7a01c05c53
albayo <albayo@unirioja.es>
2021-03-15 12:59:17 +0100

    Errores

--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -40,5 +40,26 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     new  PopulateDbAsync( INSTANCE ).execute();
                 }
             };
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
 
+        private final WordDao mDao;
+        String [] words = {"dolphin", "crocodile", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            mDao.deleteAll();
+
+            for( int i = 0; i <= words.length - 1; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
+    }
 }
